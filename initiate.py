e5d5a2243fed60e587ed21a1e2959037692b06fc
Xavier Collantes <xcollantes@users.noreply.github.com>
2022-01-27 22:15:35 -0800

    Update initiate.py

--- initiate.py
+++ initiate.py
@@ -1,5 +1,10 @@
+import requests
+
+
 def main():
     print("Hello there!! This is a text.")
+    res = requests.get("https://catfact.ninja/fact")
+    print(res.text)
     
 if __name__ == "__main__":
     main()
